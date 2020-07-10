4fecb38a668f2c48ac902641861f27fbb065c71d
Sarunas Verbus <sarunas@verbus.lt>
2020-07-10 15:47:35 +0200

    added buffered reader for people database

--- src/lt/verbus/Main.java
+++ src/lt/verbus/Main.java
@@ -1,8 +1,23 @@
 package lt.verbus;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Map<Integer, Person> people = new HashMap<>();
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get("src/databases/people.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
