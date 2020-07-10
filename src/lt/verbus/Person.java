3d5fb895db1776b67cecb95396a595f8aad73bba
Sarunas Verbus <sarunas@verbus.lt>
2020-07-10 21:24:05 +0200

    sentMoney, receivedMoney fields added together with methods for transaction handling.

--- src/lt/verbus/Person.java
+++ src/lt/verbus/Person.java
@@ -4,11 +4,23 @@ public class Person {
     int id;
     String name;
     String surname;
+    double receivedMoney;
+    double sentMoney;
 
     public Person(int id, String name, String surname) {
         this.id = id;
         this.name = name;
         this.surname = surname;
+        this.receivedMoney = 0;
+        this.sentMoney = 0;
+    }
+
+    public void transactionIn(double amount) {
+        this.receivedMoney = receivedMoney + amount;
+    }
+
+    public void transactionOut(double amount) {
+        this.sentMoney = sentMoney + amount;
     }
 
     @Override
