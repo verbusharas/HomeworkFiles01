2c17972e5c230bc1d0cf6eb0ca9b8ca765db1280
Sarunas Verbus <sarunas@verbus.lt>
2020-07-10 15:48:05 +0200

    added constructor and overrided toString method

--- src/lt/verbus/Person.java
+++ src/lt/verbus/Person.java
@@ -5,4 +5,14 @@ public class Person {
     String name;
     String surname;
 
+    public Person(int id, String name, String surname) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d | %s %s", id, name, surname);
+    }
 }
